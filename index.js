f285491b08e1efb24f71316b88300992ac3a57b7
Aelitta Ezugbaya <aelittae@gmail.com>
2018-01-23 11:02:15 +0200

    added function for form, it is not working correctly yet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let ctx = document.getElementById("myChart").getContext('2d');
 let labelsC = ["Food", "Appartment", "Car"];
-let value = [12, 19, 3];
+let values = [12, 19, 3];
 let colors =[
     'rgba(255, 99, 132, 1)',
     'rgba(54, 162, 235,1)',
@@ -10,13 +10,26 @@ let colors =[
     'rgba(255, 159, 64, 1)',
     'rgba(0, 0, 139, 1)'
 ];
+
+let form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let spends = document.getElementById('spends').value;
+    let value = document.getElementById('value').value;
+    console.log(spends +' '+ value);
+    labelsC.push(spends);
+    values.push(value);
+    console.log(labelsC)
+    form.reset()
+
+})
 let myChart = new Chart(ctx, {
     type: 'pie',
     data: {
         labels: labelsC,
         datasets: [{
             label: '# of Votes',
-            data: value,
+            data: values,
             backgroundColor: colors,
             borderColor: colors,
             borderWidth: 1
